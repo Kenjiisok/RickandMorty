34f856a381c4b6850f4a629ce1c53b3ebb844df5
Kenjiisok <dudu.hirose@hotmail.com>
2024-04-08 16:37:02 -0300

    fazendo a responsividade

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const menuBtn = document.querySelector('.menu-btn');
+    const dropdownMenu = document.querySelector('.dropdown-menu');
+  
+    menuBtn.addEventListener('click', function () {
+      dropdownMenu.classList.toggle('show');
+    });
+  });
+  
